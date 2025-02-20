a3d3fd675581255016a7e9bc707ce359bdb4e645
Adrian R. Anaya <142830862+ADI-Trickster@users.noreply.github.com>
2025-02-19 20:58:04 -0600

    Add files via upload

--- camera_2d.gd
+++ camera_2d.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position.y = clamp(global_position.y, -100, 430)
+	global_position.y = clamp(global_position.y, -200, 430)
 	#pass
